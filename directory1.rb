f9870c6917919a226acacd64a7f55a8d93171e1b
Farya <farya.hsn@gmail.com>
2021-07-22 07:16:48 +0200

    fileopenRuby

--- directory1.rb
+++ directory1.rb
@@ -173,6 +173,16 @@ def input_students
   print_footer students
   end
   
+def save_students
+    file = File.open("Students.csv", "w")
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
+end
+
   def interactive_menu
     loop do
       print_menu
