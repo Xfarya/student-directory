116422bdd221e025928035910009bf9966f561cd
Farya <farya.hsn@gmail.com>
2021-07-22 17:06:38 +0200

    step10

--- directory.rb
+++ directory.rb
@@ -1,30 +1,28 @@
 
+    @students = []
+
     def interactive_menu
-        students = []
         loop do
+            print_menu
+          # 2. read the input and save it into a variable
+          selection = gets.chomp
+          # 3. do what the user has asked
+          process(gets.chomp)
+        end
+      end
+
+      def print_menu
           # 1. print the menu
           puts "1. Input the students"
           puts "2. Show the students"
           puts "9. Exit" 
-          # 2. read the input and save it into a variable
-          selection = gets.chomp
-          # 3. do what the user has asked
-          case selection
-          when "1"
-            students = input_students
-          when "2"
-            print_header
-            print(students)
-            print_footer(students)
-          when "9"
-            exit
-          else
-            puts "I don't know what you meant, try again"
-          end
-        end
       end
 
-
+      def show_students
+        print_header
+        print_students_list
+        print_footer
+      end
 
 # students in an array
 def input_students
@@ -33,26 +31,26 @@ def input_students
 students = []
 name = gets.chomp
 while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
 end
 students
 end
 
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-]
+# students = [
+#     {name: "Dr. Hannibal Lecter", cohort: :november},
+#     {name: "Darth Vader", cohort: :november},
+#     {name: "Nurse Ratched", cohort: :november},
+#     {name: "Michael Corleone", cohort: :november},
+#     {name: "Alex DeLarge", cohort: :november},
+#     {name: "The Wicked Witch of the West", cohort: :november},
+#     {name: "Terminator", cohort: :november},
+#     {name: "Freddy Krueger", cohort: :november},
+#     {name: "The Joker", cohort: :november},
+#     {name: "Joffrey Baratheon", cohort: :november},
+#     {name: "Norman Bates", cohort: :november}
+# ]
 
 
 # first we print list of students
@@ -61,15 +59,28 @@ puts "The students of Villains Academy"
 puts "_______________"
 end
 
-def print(names)
-names.each do |student|
+def print_students_list
+@students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
 end
 end
 
 # then we print total number of students
-def print_footer(names)
-puts "Overall, we have #{names.count} great students"
+def print_footer
+puts "Overall, we have #{@students.count} great students"
+end
+
+def process(selection)
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+     show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
 end
 
 interactive_menu
